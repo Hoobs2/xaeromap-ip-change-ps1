e5465b490c10e1f7ebbf771d1552c1309b7787b0
Davey <41457078+Hoobs2@users.noreply.github.com>
2024-01-01 21:24:29 -0800

    modified:   Rename-XaeroFolders.ps1         Command window will now stay open after outputting the new folders to confirm change.

--- Rename-XaeroFolders.ps1
+++ Rename-XaeroFolders.ps1
@@ -10,3 +10,7 @@ Get-ChildItem -Path $env:USERPROFILE"\curseforge\minecraft\Instances\Prodigium R
 
 #Rename Map data folder with entered IP.
 Get-ChildItem -Path $env:USERPROFILE"\curseforge\minecraft\Instances\Prodigium Reforged\XaeroWorldMap\" -Directory -Filter "Multiplayer_*" | Rename-Item -NewName "Multiplayer_$NewIP"
+
+Write-Output Get-ChildItem -Path $env:USERPROFILE"\curseforge\minecraft\Instances\Prodigium Reforged\XaeroWaypoints\" -Directory -Filter "Multiplayer_*"
+Write-Output Get-ChildItem -Path $env:USERPROFILE"\curseforge\minecraft\Instances\Prodigium Reforged\XaeroWorldMap\" -Directory -Filter "Multiplayer_*"
+Read-Host -Prompt "Press Enter to exit"
