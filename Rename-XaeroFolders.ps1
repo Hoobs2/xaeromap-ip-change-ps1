3ef536a7afdb46817108ab8162359ff416855041
Davey <41457078+Hoobs2@users.noreply.github.com>
2024-01-04 19:47:39 -0800

    modified:   Rename-XaeroFolders.ps1
    Fixed the Return of the confirmed name changes, made it a bit neater by implementing hashtable.

--- Rename-XaeroFolders.ps1
+++ Rename-XaeroFolders.ps1
@@ -3,14 +3,28 @@ Davey
 2024/01/01
 #>
 
+# Specifyed the known part of the file name
+$partialFileName = "Multiplayer_"
+
+# Define folder paths
+$folderPaths = @("$env:USERPROFILE\curseforge\minecraft\Instances\Prodigium Reforged\XaeroWaypoints\", "$env:USERPROFILE\curseforge\minecraft\Instances\Prodigium Reforged\XaeroWorldMap\")
+
 [String]$NewIP = Read-Host "Please Enter IP "
 
-#Rename Waypoints folder with entered IP.
-Get-ChildItem -Path $env:USERPROFILE"\curseforge\minecraft\Instances\Prodigium Reforged\XaeroWaypoints\" -Directory -Filter "Multiplayer_*" | Rename-Item -NewName "Multiplayer_$NewIP"
+# Find matching files in each directory
+foreach ($path in $folderPaths) {
 
-#Rename Map data folder with entered IP.
-Get-ChildItem -Path $env:USERPROFILE"\curseforge\minecraft\Instances\Prodigium Reforged\XaeroWorldMap\" -Directory -Filter "Multiplayer_*" | Rename-Item -NewName "Multiplayer_$NewIP"
+    #Rename Waypoints folder with entered IP.
+    Get-ChildItem -Path $path -Directory -Filter "$partialFileName*" | Rename-Item -NewName "Multiplayer_$NewIP"
 
-Write-Output Get-ChildItem -Path $env:USERPROFILE"\curseforge\minecraft\Instances\Prodigium Reforged\XaeroWaypoints\" -Directory -Filter "Multiplayer_*"
-Write-Output Get-ChildItem -Path $env:USERPROFILE"\curseforge\minecraft\Instances\Prodigium Reforged\XaeroWorldMap\" -Directory -Filter "Multiplayer_*"
+    $matchingFolder = Get-ChildItem -Path $path -Directory -Filter "$partialFileName*" -ErrorAction SilentlyContinue | Select-Object -First 1 -ErrorAction SilentlyContinue
+
+    if ($matchingFolder) {
+        # Output the name of the first matching file in the directory
+        Write-Host ("Matching file name in {0}: {1}" -f $path, $matchingFolder.Name)
+    } else {
+        Write-Host "No files matching '$partialFileName' found in $path."
+    }
+}
+
 Read-Host -Prompt "Press Enter to exit"
